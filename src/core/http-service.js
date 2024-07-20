8dc3c165f277941886ff1aee8231537c123ac2f0
amirhoseinshagholani <sh.amirhosein@gmail.com>
2024-07-20 12:37:10 +0330

    fixed url

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 // const BASE_URL = "http://185.126.8.108/NOMS-BE/API";
-const BASE_URL = "http://localhost:3000/nekatel/api";
+const BASE_URL = "http://185.126.8.101:3000/nekatel/api";
 
 export const httpService = axios.create({
     baseURL: BASE_URL
